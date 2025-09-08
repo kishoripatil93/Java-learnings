c6800b8c2608db6fbb9f1bcfdb102ee67752d808
kishoripatil93 <kishoripatil93@gmail.com>
2025-09-08 13:27:23 -0700

    Added Day 1: Hello World program

---
+++ Java-Day1/HelloWorld.java
@@ -0,0 +1,7 @@
+// This is a simple Java program to print Hello World
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
